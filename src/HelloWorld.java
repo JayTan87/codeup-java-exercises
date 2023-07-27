3c73cf10908bff48475f78c23f00fec83ef43f57
Jeremy Tanner <tanner.jeremy.j@gmail.com>
2023-07-27 16:42:38 -0500

    added helloworld exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        System.out.println(" World!");
+
+        int favoriteNumber = 13;
+        boolean whateverIWant = true;
+        double myNumber = 3.14;
+        String myString = "Yes";
+        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+//        int x = 4;
+//            x +=  5;
+//        System.out.println(x);
+//        int x = 3;
+//        int y = 4;
+//        y *=  x;
+//        System.out.println(y);
+        int x = 10;
+        int y = 2;
+        x /=  y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+}
